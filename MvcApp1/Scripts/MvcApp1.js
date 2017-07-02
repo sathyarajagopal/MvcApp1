b8e949e3add697077c26e524d73210475f5e5047
sathyarajagopal <smartaxgenius@gmail.com>
2017-07-02 17:20:34 -0400

    Add project files.

---
+++ MvcApp1/Scripts/MvcApp1.js
@@ -0,0 +1,2 @@
+﻿$("#left-content").append("Version - Default<br/>");
+$("#right-content").append("Path - D:\\Github\\MvcApp1\\MvcApp1\\Scripts\\MvcApp1.js<br/>");
---
+++ MvcApp1/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-3.1.1.js" />
+/// <reference path="jquery-3.1.1.slim.js" />
+/// <reference path="mvcapp1.js" />
